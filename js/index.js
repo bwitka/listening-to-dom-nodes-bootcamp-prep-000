90fbad8d740aef838b8c2980884fc5c718cc54d4
BEAU WITKA <bwitka@gmail.com>
2018-12-31 20:49:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -28,3 +28,10 @@ function(e) {
 
 let divs = document.querySelectorAll('div');
 
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
