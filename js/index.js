7ddcf3a1dfc2ebd43ac28d2cf48c5ed791d705ab
BEAU WITKA <bwitka@gmail.com>
2018-12-31 21:05:14 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -49,5 +49,11 @@ function(e) {
 const divs = document.querySelectorAll('div');
 
 function bubble(e) {
+  e.stopPropagation();
   
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
 }
